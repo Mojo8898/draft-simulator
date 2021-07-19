65a6d1cc0782ed074774bf0813d1707503e78343
Eikix <66871571+Eikix@users.noreply.github.com>
2021-07-19 14:48:27 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+
+const championImageURL_start = 'https://ddragon.leagueoflegends.com/cdn/'
+const championImageURL_middle = '/img/champion/'
+const championImageURL_end = '.png'
+const leagueVersion = process.env.LEAGUE_VERSION
+
+
+
+function Home() {
+  return (
+    <div>
+      <h1> hi </h1>
+    </div>
+  )
+}
+
+export default Home
+
+
+export async function getStaticProps(context) {
+
+  const LOL_DATA = await axios.get(`${championImageURL_start}${leagueVersion}/data/en_US/champion.json`);
+  const {data} = LOL_DATA;
+
+  return {
+    props: {
+      data,
+    }, // will be passed to the page component as props
+  }
+}
